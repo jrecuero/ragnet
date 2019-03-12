9fb118033fd357ec782106305dbbedc22425ff91
Jose Carlos Recuero Arias <jose.recuero@gmail.com>
2019-03-12 12:40:29 -0700

    Damagers stats collected. Adding pandas

--- engine.py
+++ engine.py
@@ -1,6 +1,7 @@
 import random
 from collections import Counter
-from base import new_damager, new_defenser
+from base import new_defenser
+from damagers import new_damager
 
 
 class Engine:
@@ -38,101 +39,144 @@ class Engine:
         for unit in self.units:
             print("winner: {}".format(unit))
         print("{}".format([(t.name, t.life.native) for t in self.units[0].targets]))
+        stats = {}
+        pds = []
+        for k, dmgs in self.units[0]._targets.items():
+            stats.setdefault(k.klass, []).extend(dmgs)
+            pds.extend(dmgs)
+        for k, dmgs in stats.items():
+            print(k)
+            for d in dmgs:
+                print("\t{}".format(d))
+        import pandas
+
+        df = pandas.DataFrame(pds)
+        df = df[["klass", "target", "damager", "damage", "slash", "blunt", "pierce"]]
+        print(df)
 
 
 def main():
     eng = Engine()
     from unit import new_unit
 
-    p = new_unit(
-        "player",
-        random.randint(50, 100),
-        new_damager(
-            "weapon",
-            random.randint(10, 20),
-            slash=random.randint(25, 50),
-            blunt=random.randint(25, 50),
-            pierce=random.randint(25, 50),
-        ),
-        new_defenser(
-            random.randint(5, 10),
-            slash=random.randint(1, 50),
-            blunt=random.randint(1, 50),
-            pierce=random.randint(1, 50),
-        ),
-        "blue",
-        "human",
-    )
-    eng.units.append(p)
-    for x in range(random.randint(5, 10)):
-        life = random.randint(10, 20)
-        u = new_unit(
-            "demon{}".format(x),
-            life,
+    def generate_player():
+        return new_unit(
+            "player",
+            random.randint(100, 200),
             new_damager(
                 "weapon",
-                random.randint(1, 10),
-                slash=random.randint(25, 50),
-                blunt=random.randint(1, 25),
-                pierce=random.randint(25, 50),
+                random.randint(10, 20),
+                # slash=random.randint(25, 50),
+                # blunt=random.randint(25, 50),
+                # pierce=random.randint(25, 50),
+                slash=50,
+                blunt=50,
+                pierce=50,
             ),
             new_defenser(
-                random.randint(1, 5),
+                random.randint(5, 10),
                 slash=random.randint(1, 50),
-                blunt=random.randint(1, 10),
-                pierce=random.randint(1, 50),
-            ),
-            "red",
-            "demon",
-            life + 10,
-        )
-        eng.units.append(u)
-    for x in range(random.randint(5, 10)):
-        life = random.randint(10, 20)
-        u = new_unit(
-            "fish{}".format(x),
-            life,
-            new_damager(
-                "weapon",
-                random.randint(1, 10),
-                slash=random.randint(1, 25),
-                blunt=random.randint(25, 50),
-                pierce=random.randint(25, 50),
-            ),
-            new_defenser(
-                random.randint(1, 5),
-                slash=random.randint(1, 10),
                 blunt=random.randint(1, 50),
                 pierce=random.randint(1, 50),
             ),
-            "green",
-            "fish",
-            life + 10,
+            "blue",
+            "human",
         )
-        eng.units.append(u)
-    for x in range(random.randint(5, 10)):
-        life = random.randint(10, 20)
-        u = new_unit(
-            "bird{}".format(x),
-            life,
-            new_damager(
-                "weapon",
-                random.randint(1, 10),
-                slash=random.randint(25, 50),
-                blunt=random.randint(25, 50),
-                pierce=random.randint(1, 10),
-            ),
-            new_defenser(
-                random.randint(1, 5),
-                slash=random.randint(1, 50),
-                blunt=random.randint(1, 50),
-                pierce=random.randint(0, 0),
-            ),
-            "yellow",
-            "bird",
-            life + 10,
-        )
-        eng.units.append(u)
+
+    def generate_demons():
+        demons = []
+        for x in range(random.randint(5, 10)):
+            life = random.randint(40, 50)
+            u = new_unit(
+                "demon{}".format(x),
+                life,
+                new_damager(
+                    "weapon",
+                    random.randint(1, 10),
+                    slash=random.randint(25, 50),
+                    blunt=random.randint(1, 25),
+                    pierce=random.randint(25, 50),
+                ),
+                new_defenser(
+                    random.randint(1, 5),
+                    # slash=random.randint(1, 50),
+                    # blunt=random.randint(1, 10),
+                    # pierce=random.randint(1, 50),
+                    slash=100,
+                    blunt=0,
+                    pierce=100,
+                ),
+                "red",
+                "demon",
+                life + 10,
+            )
+            demons.append(u)
+        return demons
+
+    def generate_fishes():
+        fishes = []
+        for x in range(random.randint(5, 10)):
+            life = random.randint(20, 40)
+            u = new_unit(
+                "fish{}".format(x),
+                life,
+                new_damager(
+                    "weapon",
+                    random.randint(1, 10),
+                    slash=random.randint(1, 25),
+                    blunt=random.randint(25, 50),
+                    pierce=random.randint(25, 50),
+                ),
+                new_defenser(
+                    random.randint(1, 5),
+                    # slash=random.randint(1, 10),
+                    # blunt=random.randint(1, 50),
+                    # pierce=random.randint(1, 50),
+                    slash=0,
+                    blunt=100,
+                    pierce=100,
+                ),
+                "green",
+                "fish",
+                life + 10,
+            )
+            fishes.append(u)
+        return fishes
+
+    def generate_birds():
+        birds = []
+        for x in range(random.randint(5, 10)):
+            life = random.randint(25, 40)
+            u = new_unit(
+                "bird{}".format(x),
+                life,
+                new_damager(
+                    "weapon",
+                    random.randint(1, 10),
+                    slash=random.randint(25, 50),
+                    blunt=random.randint(25, 50),
+                    pierce=random.randint(1, 10),
+                ),
+                new_defenser(
+                    random.randint(1, 5),
+                    # slash=random.randint(1, 50),
+                    # blunt=random.randint(1, 50),
+                    # pierce=random.randint(0, 0),
+                    slash=100,
+                    blunt=100,
+                    pierce=0,
+                ),
+                "yellow",
+                "bird",
+                life + 10,
+            )
+            birds.append(u)
+        return birds
+
+    eng.units.append(generate_player())
+    eng.units.extend(generate_demons())
+    eng.units.extend(generate_fishes())
+    eng.units.extend(generate_birds())
     while eng.check_for_end():
         eng.print_units()
         eng.turn()
--- unit.py
+++ unit.py
@@ -1,6 +1,7 @@
 from collections import OrderedDict
 import random
-from base import Base, Stat, Damager, Defenser
+from base import Base, Stat, Defenser
+from damagers import Damager, new_scan
 
 
 class Unit(Base):
@@ -16,7 +17,8 @@ class Unit(Base):
         self.xp_for_kill: int = 0
         self.platoon: str = None
         self.klass: str = None
-        self._targets = []
+        self._targets = OrderedDict()
+        self._scan = None
 
     def __str__(self):
         return "[{:2}] | {:8} ({:2}|{:3}) | {:8} | {:8} | {:3} | {} | {} -> t:{:8}".format(
@@ -29,13 +31,12 @@ class Unit(Base):
             self.life.raw,
             self.damager,
             self.defenser,
-            self._targets[-1].name if len(self._targets) else "None",
+            list(self._targets.keys())[-1].name if len(self._targets) else "None",
         )
 
     @property
     def targets(self):
-        t = OrderedDict.fromkeys(self._targets)
-        return list(t.keys())
+        return list(self._targets.keys())
 
     def is_alive(self):
         return self.life.raw > 0
@@ -50,11 +51,14 @@ class Unit(Base):
             self.level += 1
 
     def damage_to(self, target):
-        self._targets.append(target)
-        # pdmg = self.pdmg.raw - target.pdef.raw
-        pdmg = self.damager.damage_to(target.defenser)
-        if pdmg > 0:
-            target.life.raw -= pdmg
+        if target in self._targets:
+            dmg = self.damager.damage_to(target.defenser)
+            if dmg["damage"] > 0:
+                target.life.raw -= dmg["damage"]
+        else:
+            dmg = self._scan.damage_to(target.defenser)
+        dmg.update({"klass": target.klass, "target": target.name})
+        self._targets.setdefault(target, []).append(dmg)
         if not target.is_alive():
             self.xp += target.xp_for_kill
         self.level_up()
@@ -70,4 +74,5 @@ def new_unit(name, life, damager, defenser, platoon, klass, xp=10) -> Unit:
     unit.platoon = platoon
     unit.klass = klass
     unit.xp_for_kill = xp
+    unit._scan = new_scan(damager.raw)
     return unit
